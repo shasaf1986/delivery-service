9e5d9d48d51c71b4b739ee36e40dbced79077418
Asaf Shmueli <asaf@play-them.com>
2019-11-25 18:46:06 +0200

    add material ui

---
+++ src/components/app/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const App: React.FC = () => (
+  <>
+    <CssBaseline />
+    <div className="App">
+    </div>
+  </>
+);
+
+export default App;
