2dcadeff6312d5b5b0218bf166132ce5785fe897
Asaf Shmueli <asaf@play-them.com>
2019-11-28 23:55:00 +0200

    fix resultMessage

--- src/components/routeCalculator/routeCalculator.tsx
+++ src/components/routeCalculator/routeCalculator.tsx
@@ -29,38 +29,32 @@ const useResultMessage = (
   path: string[],
   selectedTab: number,
   maxStops: number,
-) => {
-  const [resultMessage, setResultMessage] = useState<string | null>(null);
-  useMemo(() => {
-    if (path.length < 2) {
-      setResultMessage('');
-      return;
+) => useMemo(() => {
+  if (path.length < 2) {
+    return '';
+  }
+  switch (selectedTab) {
+    case 0: {
+      const cost = calculator.getDeliveryCost(path);
+      return cost !== null ? `The cost is ${cost}` : 'No such route';
     }
-    // eslint-disable-next-line default-case
-    switch (selectedTab) {
-      case 0: {
-        const cost = calculator.getDeliveryCost(path);
-        setResultMessage(cost !== null ? `The cost is ${cost}` : 'No such route');
-        break;
-      }
-      case 1: {
-        const [from, to] = path;
-        const count = calculator.getPossiblePathsCount(from, to, {
-          maxStops: maxStops > 0 ? maxStops : undefined,
-        });
-        setResultMessage(`The possible routes are ${count}`);
-        break;
-      }
-      case 2: {
-        const [from, to] = path;
-        const cost = calculator.getShortestPathLength(from, to);
-        setResultMessage(cost !== null ? `The cost for the cheapest delivery route is ${cost}` : 'No such route');
-        break;
-      }
+    case 1: {
+      const [from, to] = path;
+      const count = calculator.getPossiblePathsCount(from, to, {
+        maxStops: maxStops > 0 ? maxStops : undefined,
+      });
+      return `The possible routes are ${count}`;
     }
-  }, [path, selectedTab, maxStops, calculator]);
-  return resultMessage;
-};
+    case 2: {
+      const [from, to] = path;
+      const cost = calculator.getShortestPathLength(from, to);
+      return cost !== null ? `The cost for the cheapest delivery route is ${cost}` : 'No such route';
+    }
+    default: {
+      throw new Error('unknown case');
+    }
+  }
+}, [path, selectedTab, maxStops, calculator]);
 
 const useRest = (
   setCity: (value: string) => void,
