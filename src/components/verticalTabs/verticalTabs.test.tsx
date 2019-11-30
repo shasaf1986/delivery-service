59a57ccf7e8dc119ca18e4c3ec841bd3774a0e81
Asaf Shmueli <asaf@play-them.com>
2019-11-30 22:45:59 +0200

    fix describe name

--- src/components/verticalTabs/verticalTabs.test.tsx
+++ src/components/verticalTabs/verticalTabs.test.tsx
@@ -2,7 +2,7 @@ import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import VerticalTabs from './verticalTabs';
 
-describe('verticalTabs', () => {
+describe('VerticalTabs', () => {
   test('should call onChange with correct new tab after click another tab', () => {
     const onChange = jest.fn<void, number[]>();
     const { getByText } = render(
