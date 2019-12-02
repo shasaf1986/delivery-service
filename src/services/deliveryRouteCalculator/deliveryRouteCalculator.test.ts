64f4b2a3f445edff2be6109b289b3b7b421ef3c5
Asaf Shmueli <asaf@play-them.com>
2019-12-03 00:13:53 +0200

    refactor test

--- src/services/deliveryRouteCalculator/deliveryRouteCalculator.test.ts
+++ src/services/deliveryRouteCalculator/deliveryRouteCalculator.test.ts
@@ -9,73 +9,34 @@ describe('DeliveryRouteCalculator', () => {
     });
   });
   describe('getPossibleDeliveryRoutes', () => {
-    test('should return correct number of paths', () => {
-      const params = [{
-        from: 'E',
-        to: 'D',
-        maxStops: 4,
-        result: 4,
-      }, {
-        from: 'E',
-        to: 'E',
-        maxStops: Infinity,
-        result: 5,
-      }];
-      params.forEach(({
-        from, to, maxStops, result,
-      }) => {
-        const count = calculator.getPossibleDeliveryRoutes(from, to, {
-          maxStops,
-        });
-        expect(count).toBe(result);
+    test.each<any>([
+      ['E', 'D', 4, 4],
+      ['E', 'E', Infinity, 5],
+    ])('should return correct number of paths', (from: string, to: string, maxStops: number, expected: number) => {
+      const count = calculator.getPossibleDeliveryRoutes(from, to, {
+        maxStops,
       });
+      expect(count).toBe(expected);
     });
   });
   describe('getDeliveryCost', () => {
-    test('should return correct cost for routes', () => {
-      const params = [
-        {
-          route: ['A', 'B', 'E'],
-          cost: 4,
-        },
-        {
-          route: ['A', 'D'],
-          cost: 10,
-        },
-        {
-          route: ['E', 'A', 'C', 'F'],
-          cost: 8,
-        },
-        {
-          route: ['A', 'D', 'F'],
-          cost: null,
-        },
-      ];
-      params.forEach(({ route, cost }) => {
-        const actualCost = calculator.getDeliveryCost(route);
-        expect(actualCost).toBe(cost);
-      });
+    test.each<any>([
+      [['A', 'B', 'E'], 4],
+      [['A', 'D'], 10],
+      [['E', 'A', 'C', 'F'], 8],
+      [['A', 'D', 'F'], null],
+    ])('should return correct cost for routes', (path: string[], expected: number | null) => {
+      const actualCost = calculator.getDeliveryCost(path);
+      expect(actualCost).toBe(expected);
     });
   });
   describe('getCheapestDeliveryRoute', () => {
-    test('should return correct cost for routes', () => {
-      const params = [
-        {
-          from: 'E',
-          to: 'D',
-          totalWeight: 9,
-        },
-        {
-
-          from: 'E',
-          to: 'E',
-          totalWeight: 6,
-        },
-      ];
-      params.forEach(({ from, to, totalWeight }) => {
-        const length = calculator.getCheapestDeliveryRoute(from, to)!;
-        expect(length).toBe(totalWeight);
-      });
+    test.each<any>([
+      ['E', 'D', 9],
+      ['E', 'E', 6],
+    ])('should return correct cost for routes', (from: string, to: string, expected: number) => {
+      const cost = calculator.getCheapestDeliveryRoute(from, to)!;
+      expect(cost).toBe(expected);
     });
   });
 });
