f7ac4aaaa5d1f5a156cf27957b3b4f31e92c9731
Asaf Shmueli <asaf@play-them.com>
2019-12-03 00:27:32 +0200

    refactor test

--- src/components/path/path.test.tsx
+++ src/components/path/path.test.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Path from './path';
 
 describe('Path', () => {
-  test('should render correct path', () => {
-    for (let i = 0; i < 3; i += 1) {
-      cleanup();
-      const path = [];
-      for (let j = 0; j < i; j += 1) {
-        path.push(`NODE_${j}`);
-      }
-      const { queryAllByText } = render(<Path path={path} />);
-      // should render paths if path.length > 0
-      expect(queryAllByText(/NODE/i).length).toBe(i);
-      // should render placeholder paths if path.length < 2
-      expect(queryAllByText('?').length).toBe(i < 2 ? 2 - i : 0);
-    }
+  test.each([
+    [0],
+    [1],
+    [2],
+  ])('should render correct path', (numberOfNodes) => {
+    const path = Array.from({ length: numberOfNodes }, (_, index) => `NODE_${index}`);
+    const { queryAllByText } = render(<Path path={path} />);
+    // should render paths if path.length > 0
+    expect(queryAllByText(/NODE/i).length).toBe(numberOfNodes);
+    // should render placeholder paths if path.length < 2
+    expect(queryAllByText('?').length).toBe(numberOfNodes < 2 ? 2 - numberOfNodes : 0);
   });
 });
