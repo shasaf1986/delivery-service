c424816b4194810c4f626466664b5f8650b7d1b8
Asaf Shmueli <asaf@play-them.com>
2019-12-03 02:16:36 +0200

    fix test names

--- src/services/deliveryRouteCalculator/deliveryRouteCalculator.test.ts
+++ src/services/deliveryRouteCalculator/deliveryRouteCalculator.test.ts
@@ -12,7 +12,7 @@ describe('DeliveryRouteCalculator', () => {
     test.each<any>([
       ['E', 'D', 4, 4],
       ['E', 'E', Infinity, 5],
-    ])('should return correct number of paths', (from: string, to: string, maxStops: number, expected: number) => {
+    ])('should return number of paths', (from: string, to: string, maxStops: number, expected: number) => {
       const count = calculator.getPossibleDeliveryRoutes(from, to, {
         maxStops,
       });
@@ -25,7 +25,7 @@ describe('DeliveryRouteCalculator', () => {
       [['A', 'D'], 10],
       [['E', 'A', 'C', 'F'], 8],
       [['A', 'D', 'F'], null],
-    ])('should return correct cost for routes', (path: string[], expected: number | null) => {
+    ])('should return cost of the path', (path: string[], expected: number | null) => {
       const actualCost = calculator.getDeliveryCost(path);
       expect(actualCost).toBe(expected);
     });
@@ -35,7 +35,7 @@ describe('DeliveryRouteCalculator', () => {
       ['E', 'D', 9],
       ['E', 'E', 6],
       ['A', 'G', null],
-    ])('should return correct cost for routes', (from: string, to: string, expected: number) => {
+    ])('should return the cheapest delivery route for the path', (from: string, to: string, expected: number) => {
       const cost = calculator.getCheapestDeliveryRoute(from, to);
       expect(cost).toBe(expected);
     });
